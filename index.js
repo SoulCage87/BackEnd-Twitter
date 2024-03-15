f8576e3e2b348c074ac67d0d7c7ab95b276a4460
Samuel Galo <samuelgalo1625@gmail.com>
2024-03-15 14:34:25 -0600

    Commit

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from 'express'
+import cors from 'cors'
 import { user } from './routes/routeUser.js';
 import { publicacion } from './routes/routePublicaciones.js';
 import { comentario } from './routes/routeComentario.js';
 const app = express();
 const port = 4000;
 
+const corsOptions = {
+    origin : 'http://localhost:5173', 
+    credentials : true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
 
 
 //MiddleWares
 app.use(express.json());
+app.use(cors(corsOptions));
 
+//Rutas
 app.use('/api/usuario', user)
 app.use('/api/publicacion', publicacion)
 app.use('/api/comentario',comentario)
